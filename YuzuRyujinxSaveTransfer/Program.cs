bcdba3bbe728d6ca14e3edb635bb2e494175be11
Exp <17885980+exp111@users.noreply.github.com>
2022-12-08 18:53:58 +0100

    Transfer saves

--- YuzuRyujinxSaveTransfer/Program.cs
+++ YuzuRyujinxSaveTransfer/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System.CommandLine;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -27,16 +29,32 @@ internal class Program
         var ryujinxPath = new Option<DirectoryInfo?>(
             name: "--ryujinx-path",
             description: "Ryujinx's file path");
+        var transferTitles = new Option<string[]?>(
+            name: "--titles",
+            description: "Title IDs that should be transfered")
+        {
+            AllowMultipleArgumentsPerToken = true
+        };
+        var transferAll = new Option<bool>(
+            name: "--all",
+            getDefaultValue: () => false,
+            description: "Transfers all saves.");
+        var source = new Option<string?>(
+            name: "--source",
+            description: "Source of the saves to be transfered").FromAmong("yuzu", "ryujinx");
 
         var rootCommand = new RootCommand("Transfers ");
         rootCommand.AddOption(yuzuPath);
         rootCommand.AddOption(ryujinxPath);
+        rootCommand.AddOption(yuzuUser);
+        rootCommand.AddOption(transferTitles);
+        rootCommand.AddOption(transferAll);
+        rootCommand.AddOption(source);
 
         //TODO: subcommands list/transfer?
 
-        rootCommand.SetHandler((yuzu, ryujinx, yUser) =>
+        rootCommand.SetHandler((yuzu, ryujinx, yUser, titles, all, source) =>
         {
-            //ReadFile(file!);
             Console.WriteLine($"yuzu: {yuzu}, ryujinx: {ryujinx}");
             //TODO: ask interactively for missing stuff
             YuzuPath = yuzu;
@@ -52,7 +70,7 @@ internal class Program
                 {
                     Console.WriteLine($"User {user.Name} ({user.SaveCount} saves)");
                 }
-                
+
                 if (!string.IsNullOrEmpty(yUser))
                 {
                     var user = users.Find(u => u.Name.ToLower() == yUser.ToLower());
@@ -62,7 +80,7 @@ internal class Program
                 //TODO: ask interactively
                 //FIXME: remove debug test thing
                 selectedUser = users[1];
-                
+
             }
             else if (users.Count == 1)
             {
@@ -91,9 +109,43 @@ internal class Program
                 Console.WriteLine($"- TitleID {save.TitleID}, Path: {save.Path}");
             }
 
-            //TODO: ask/take from args what to transfer + overwrite? backups?
+            //TODO: ask/take from args what to transfer + delete+overwrite? backups?
+            if (string.IsNullOrEmpty(source))
+            {
+                Console.WriteLine("No source given!");
+                return;
+            }
+            var sourceSaves = source == "yuzu" ? yuzuSaves : ryujinxSaves;
+            var destSaves = source == "yuzu" ? ryujinxSaves : yuzuSaves;
+            if (all)
+            {
+                titles = sourceSaves.Select(s => (string)s.TitleID).ToArray();
+            }
+            Console.WriteLine("Titles to transfer:");
+            Console.WriteLine(string.Join(",", titles));
+            //TODO: each one: check if exists on both ends + transfer
+            foreach (var title in titles)
+            {
+                var srcSave = sourceSaves.Find(s => s.TitleID == title);
+                if (srcSave == null)
+                {
+                    Console.WriteLine($"Title {title} not found in source {source}");
+                    return;
+                }
+                var dstSave = destSaves.Find(s => s.TitleID == title);
+                if (dstSave == null)
+                {
+                    Console.WriteLine($"Title {title} not found in destination {(source == "yuzu" ? "Ryujinx" : "Yuzu")}");
+                    return;
+                }
+
+                Console.WriteLine($"Transfering {title} from {srcSave.Path} to {dstSave.Path}");
+                var sourcePath = source == "yuzu" ? YuzuSavePath : RyujinxSavePath;
+                var destPath = source == "yuzu" ? RyujinxSavePath : YuzuSavePath;
+                TransferSave(sourcePath, destPath, srcSave.Path, dstSave.Path, source); //TODO: transfer
+            }
         },
-            yuzuPath, ryujinxPath, yuzuUser); //TODO: use binders for yuzu parser?
+        yuzuPath, ryujinxPath, yuzuUser, transferTitles, transferAll, source); //TODO: use binders for yuzu parser?
 
         return await rootCommand.InvokeAsync(args);
     }
@@ -104,7 +156,7 @@ internal class Program
     {
         // fixme: DirInfo->string->DirInfo => make it into DirInfo->DirInfo?
         var baseSavePath = new DirectoryInfo(
-            Path.Combine(savePath, 
+            Path.Combine(savePath,
                 "nand/user/save/0000000000000000")); // traverse into the base user save folder
         var userDirs = baseSavePath.GetDirectories();
         List<dynamic> users = new(userDirs.Length);
@@ -112,8 +164,8 @@ internal class Program
         {
             // Add each user save dir and the number of saves inside
             users.Add(new
-            { 
-                Name = userDir.Name, 
+            {
+                Name = userDir.Name,
                 SaveCount = userDir.GetDirectories().Length,
                 FullPath = new DirectoryInfo(Path.Combine(baseSavePath.FullName, userDir.Name))
             });
@@ -149,7 +201,7 @@ internal class Program
             {
                 titleID = GetApplicationID(extraData[0]);
             }
-            
+
             saves.Add(
                 new
                 {
@@ -183,5 +235,73 @@ internal class Program
             return applicationID;
         }
         return applicationID;
+    }
+
+    // Transfers the save from $srcSavePath/srcDir to $dstSavePath/dstDir
+    public static bool TransferSave(DirectoryInfo srcSavePath, DirectoryInfo dstSavePath, string srcDir, string dstDir, string source)
+    {
+        // build save folders paths
+        var srcPath = Path.Combine(srcSavePath.FullName, srcDir);
+        var dstPath = Path.Combine(dstSavePath.FullName, dstDir);
+        // ryujinx has the save data in a subdir called 0 (the commited save dir), so add that to the yuzuPath
+        if (source == "yuzu")
+            dstPath = Path.Combine(dstPath, "0");
+        else
+            srcPath = Path.Combine(srcPath, "0");
+
+        //Console.WriteLine($"source: {srcPath}");
+        Process.Start("explorer.exe", srcPath);
+        //Console.WriteLine($"dst: {dstPath}");
+        Process.Start("explorer.exe", dstPath);
+        // sanity checks
+        if (!Path.Exists(srcPath) || !Path.Exists(dstPath))
+            return false;
+
+        // backup
+        var origPath = new DirectoryInfo(dstPath);
+        var backupPath = Path.Combine(origPath.Parent.FullName, $"{dstDir}_bak");
+        // delete old backup if it exists
+        if (Path.Exists(backupPath))
+            Directory.Delete(backupPath, true);
+        Directory.Move(dstPath, backupPath);
+        // copy the dir over
+        CopyDirectory(srcPath, dstPath, true);
+        return true;
+    }
+
+
+    // Copies a directory
+    // https://learn.microsoft.com/en-us/dotnet/standard/io/how-to-copy-directories
+    static void CopyDirectory(string sourceDir, string destinationDir, bool recursive)
+    {
+        // Get information about the source directory
+        var dir = new DirectoryInfo(sourceDir);
+
+        // Check if the source directory exists
+        if (!dir.Exists)
+            throw new DirectoryNotFoundException($"Source directory not found: {dir.FullName}");
+
+        // Cache directories before we start copying
+        DirectoryInfo[] dirs = dir.GetDirectories();
+
+        // Create the destination directory
+        Directory.CreateDirectory(destinationDir);
+
+        // Get the files in the source directory and copy to the destination directory
+        foreach (FileInfo file in dir.GetFiles())
+        {
+            string targetFilePath = Path.Combine(destinationDir, file.Name);
+            file.CopyTo(targetFilePath);
+        }
+
+        // If recursive and copying subdirectories, recursively call this method
+        if (recursive)
+        {
+            foreach (DirectoryInfo subDir in dirs)
+            {
+                string newDestinationDir = Path.Combine(destinationDir, subDir.Name);
+                CopyDirectory(subDir.FullName, newDestinationDir, true);
+            }
+        }
     }
 }
